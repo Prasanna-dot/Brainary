d8a42b7f18954deb5eb36cb8a168c9c7d08c1acc
prasanna-dot <prasannavenaksteh20.01.2k1@gmail.com>
2022-07-17 14:28:25 -0800

    Add

---
+++ db/migrate/20220717005842_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.bigint :mobile
+      t.string :email
+      t.string :password_digest
+      t.integer :batch
+      t.binary :profile_pic
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
